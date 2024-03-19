21c72d7e83f47ba18ec08437a0fff5b6a304f919
Hawthorn Emerald <hawthornemerald@Hawthorns-MBP.campus.ismanila.int>
2024-03-19 15:01:30 +0800

    changed triple quote to single quotes

--- tapmphud.py
+++ tapmphud.py
@@ -2,52 +2,48 @@
 # "ta-pum-fud"
 
 syllabus = {
-    '7 Cities': {
-        '1 Origin, Distribution, and Systems of Cities': {
+    '7 Cities': { # unit
+        '1 Orig Dist Sys Cities': { # topic
             'vocab': {
                 'Urbanization': {
                     'definition': 'the process of developing towns and cities',
-                    'notes': [],
+                    'notes': '',
                 },
                 'Site': {
                     'definition': 'physical characteristics of a place',
-                    'notes': [
-                        'includes:',
-                        ' - climate',
-                        ' - natural features, especially water'
-                    ]
+                    'notes': "includes:\n"+
+                        " - climate\n"+
+                        " - natural features, especially water"
                 },
                 'Situation': {
                     'definition': 'location of a place relative to surroundings',
-                    'notes': [
-                        'includes:',
-                        ' - proximity to natural resources',
-                        ' - proximity to other cities',
-                        ' - accessibility',
-                    ]
+                    'notes': "includes:\n"
+                        " - proximity to natural resources\n" +
+                        " - proximity to other cities\n" +
+                        " - accessibility"
                 }
             },
             'notes': [ # group relevant information together
-                """Two main factors influence location of cities: site and situation
-                """,
-                """Site and situation can also impact how cities function and grow",
- - Size of cities
-   * Εxample: Manila has trouble growing because of physical constraints
- - Economic development
-   * Example: Singapore got very wealthy due to strategic position on shipping routes
- - Political/military history
-   * Example: Istanbul as a shatterbelt""",
-                """Key trend around the world -- cities are getting larger
-Causes
- 1) Population growth
-   * People need somewhere to live
- 2) Improvements in transport and communication
-   * Allows cities to expand -- just look at Manila""",
+                "Two main factors influence location of cities: site and situation",
+                    "Site and situation can also impact how cities function and grow\n"+
+                    " - Size of cities\n"+
+                    "   * Εxample: Manila has trouble growing because of physical constraints\n"+
+                    " - Economic development\n"+
+                    "   * Example: Singapore got very wealthy due to strategic position on shipping routes\n"+
+                    " - Political/military history\n"+
+                    "   * Example: Istanbul as a shatterbelt",
+                "Key trend around the world -- cities are getting larger\n"+
+                    "Causes\n"+
+                    " 1) Population growth\n"+
+                    "   * People need somewhere to live\n"+
+                    " 2) Improvements in transport and communication\n"+
+                    "   * Allows cities to expand -- just look at Manila",
             ]
         }
     }
 }
 
+# print(syllabus['7 Cities']['1 Orig Dist Sys Cities']['notes'][1])
 
 def main():
     print("Welcome to the TAPMPHUD Syllabus Manager")
