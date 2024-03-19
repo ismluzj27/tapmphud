3ee01c253c7cbaf57fb70fd3e758fe02edf6bb56
Hawthorn Emerald <hawthornemerald@Hawthorns-MBP.campus.ismanila.int>
2024-03-19 14:39:23 +0800

    init commit

---
+++ tapmphud.py
@@ -0,0 +1,58 @@
+# tapmphud.py
+# "ta-pum-fud"
+
+syllabus = {
+    '7 Cities': {
+        '1 Origin, Distribution, and Systems of Cities': {
+            'vocab': {
+                'Urbanization': {
+                    'definition': 'the process of developing towns and cities',
+                    'notes': [],
+                },
+                'Site': {
+                    'definition': 'physical characteristics of a place',
+                    'notes': [
+                        'includes:',
+                        ' - climate',
+                        ' - natural features, especially water'
+                    ]
+                },
+                'Situation': {
+                    'definition': 'location of a place relative to surroundings',
+                    'notes': [
+                        'includes:',
+                        ' - proximity to natural resources',
+                        ' - proximity to other cities',
+                        ' - accessibility',
+                    ]
+                }
+            },
+            'notes': [ # group relevant information together
+                """Two main factors influence location of cities: site and situation
+                """,
+                """Site and situation can also impact how cities function and grow",
+ - Size of cities
+   * Εxample: Manila has trouble growing because of physical constraints
+ - Economic development
+   * Example: Singapore got very wealthy due to strategic position on shipping routes
+ - Political/military history
+   * Example: Istanbul as a shatterbelt""",
+                """Key trend around the world -- cities are getting larger
+Causes
+ 1) Population growth
+   * People need somewhere to live
+ 2) Improvements in transport and communication
+   * Allows cities to expand -- just look at Manila""",
+            ]
+        }
+    }
+}
+
+
+def main():
+    print("Welcome to the TAPMPHUD Syllabus Manager")
+
+
+if __name__ == '__main__':
+    main()
+
